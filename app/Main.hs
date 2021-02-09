86e54afda73d0cf9f58937104d81886168a9dfb2
Tech Mindful <hello@techmindful.blog>
2021-02-08 20:00:01 -0800

    Improve imports.

--- app/Main.hs
+++ app/Main.hs
@@ -4,36 +4,37 @@
 
 module Main ( main ) where
 
-import Control.Exception
-import Control.Monad.IO.Class
-import Control.Monad.Managed
-import DearImGui
-import DearImGui.OpenGL2
-import DearImGui.SDL
-import DearImGui.SDL.OpenGL
-import Graphics.GL
-import SDL
+import           Control.Exception ( bracket, bracket_ )
+import           Control.Monad.IO.Class
+import           Control.Monad.Managed ( runManaged, managed, managed_ )
+import qualified DearImGui
+import           DearImGui.OpenGL2 ( openGL2Init, openGL2Shutdown, openGL2NewFrame, openGL2RenderDrawData )
+import qualified DearImGui.SDL
+import           DearImGui.SDL ( sdl2Shutdown, sdl2NewFrame )
+import           DearImGui.SDL.OpenGL ( sdl2InitForOpenGL )
+import qualified Graphics.GL
+import qualified SDL
 
 main :: IO ()
 main = do
-  -- Initialize SDL
-  initializeAll
+
+  SDL.initializeAll
 
   runManaged do
     -- Create a window using SDL. As we're using OpenGL, we need to enable OpenGL too.
     w <- do
       let title = "Hello, Dear ImGui!"
-      let config = defaultWindow { windowGraphicsContext = OpenGLContext defaultOpenGL }
-      managed $ bracket (createWindow title config) destroyWindow
+      let config = SDL.defaultWindow { SDL.windowGraphicsContext = SDL.OpenGLContext SDL.defaultOpenGL }
+      managed $ bracket ( SDL.createWindow title config ) SDL.destroyWindow
 
     -- Create an OpenGL context
-    glContext <- managed $ bracket (glCreateContext w) glDeleteContext
+    glContext <- managed $ bracket ( SDL.glCreateContext w ) SDL.glDeleteContext
 
     -- Create an ImGui context
-    _ <- managed $ bracket createContext destroyContext
+    _ <- managed $ bracket DearImGui.createContext DearImGui.destroyContext
 
     -- Initialize ImGui's SDL2 backend
-    _ <- managed_ $ bracket_ (sdl2InitForOpenGL w glContext) sdl2Shutdown
+    _ <- managed_ $ bracket_ ( sdl2InitForOpenGL w glContext) sdl2Shutdown
 
     -- Initialize ImGui's OpenGL backend
     _ <- managed_ $ bracket_ openGL2Init openGL2Shutdown
@@ -41,36 +42,36 @@ main = do
     liftIO $ mainLoop w
 
 
-mainLoop :: Window -> IO ()
+mainLoop :: SDL.Window -> IO ()
 mainLoop w = do
   -- Process the event loop
-  untilNothingM pollEventWithImGui
+  untilNothingM DearImGui.SDL.pollEventWithImGui
 
   -- Tell ImGui we're starting a new frame
   openGL2NewFrame
   sdl2NewFrame w
-  newFrame
+  DearImGui.newFrame
 
   -- Build the GUI
-  bracket_ (begin "Hello, ImGui!") end do
+  bracket_ ( DearImGui.begin "Hello, ImGui!" ) DearImGui.end do
     -- Add a text widget
-    text "Hello, ImGui!"
+    DearImGui.text "Hello, ImGui!"
 
     -- Add a button widget, and call 'putStrLn' when it's clicked
-    button "Clickety Click" >>= \case
+    DearImGui.button "Clickety Click" >>= \case
       False -> return ()
       True  -> putStrLn "Ow!"
 
   -- Show the ImGui demo window
-  showDemoWindow
+  DearImGui.showDemoWindow
 
   -- Render
-  glClear GL_COLOR_BUFFER_BIT
+  Graphics.GL.glClear Graphics.GL.GL_COLOR_BUFFER_BIT
 
-  render
-  openGL2RenderDrawData =<< getDrawData
+  DearImGui.render
+  openGL2RenderDrawData =<< DearImGui.getDrawData
 
-  glSwapWindow w
+  SDL.glSwapWindow w
 
   mainLoop w
 
