ffd6b3e0d1ba12bbb0439fd75c4dd6c2755db925
Tech Mindful <hello@techmindful.blog>
2021-02-27 19:39:46 -0800

    Node's activity name confirm button works.

--- src/DrawNode.hs
+++ src/DrawNode.hs
@@ -11,6 +11,7 @@ import           DearImGui ( ImVec2(..) )
 import           Control.Lens ( (^.), (.~), (%~) )
 import           Control.Monad.State
 import           Data.Function ( (&) )
+import           Data.IORef ( IORef, newIORef, readIORef, writeIORef )
 import qualified Data.Map as Map
 
 
@@ -23,26 +24,48 @@ drawNode node = do
   
       pad_Y = 20
 
-      actNamePos    = node ^. #drawPos
-      actEditBtnPos = ImVec2 ( x actNamePos + 100 ) ( y actNamePos )
+      actNamePos = node ^. #drawPos
+      actEditBtnPos = ImVec2 ( x actNamePos + 170 ) ( y actNamePos )
+      actConfirmBtnPos = actEditBtnPos
 
       servNamePos = ImVec2 ( x actNamePos ) ( y actNamePos + pad_Y )
       
       cursorPosRef'  = appState ^. #cursorPosRef
       setCursorPos'' = Utils.setCursorPos' cursorPosRef'
 
-  setCursorPos'' actEditBtnPos
-  DearImGui.button ( "Edit## activity name " ++ act ^. #name ) >>= \case
-    False -> return ()
-    True  -> put $ appState & #editingActivityName .~ ( Just $ act ^. #name )
 
-  setCursorPos'' actNamePos
+  DearImGui.pushItemWidth 150
+
+
   case fmap ( == act ^. #name ) ( appState ^. #editingActivityName ) of
     Just True -> do
+      setCursorPos'' actNamePos
       DearImGui.inputText ( "##Editing Activity " ++ act ^. #name ) ( appState ^. #activityNameEditRef ) 128
-      return ()
 
-    _ -> DearImGui.text $ act ^. #name
+      setCursorPos'' actConfirmBtnPos
+      DearImGui.button ( "Confirm## activity name " ++ act ^. #name ) >>= \case
+        False -> return ()
+        True  -> do
+          newActName <- liftIO $ readIORef $ appState ^. #activityNameEditRef
+
+          let f_UpdateNodes :: [ Node ] -> [ Node ]
+              f_UpdateNodes nodes =
+                map ( \node ->
+                  if node ^. #activity . #name == act ^. #name then
+                    node & #activity . #name .~ newActName
+                  else node
+                ) nodes
+
+          put $ appState & #appData . #nodes %~ f_UpdateNodes
+
+    _ -> do
+      setCursorPos'' actNamePos
+      DearImGui.text $ act ^. #name
+
+      setCursorPos'' actEditBtnPos
+      DearImGui.button ( "Edit## activity name " ++ act ^. #name ) >>= \case
+        False -> return ()
+        True  -> put $ appState & #editingActivityName .~ ( Just $ act ^. #name )
 
   setCursorPos'' servNamePos
   isServComboOpen <- DearImGui.beginCombo ( "##Service Combo For " ++ act ^. #name ) ( act ^. #service )
@@ -61,4 +84,7 @@ drawNode node = do
 
       put =<< ( liftIO $ execStateT drawServSels appState )
       DearImGui.endCombo
+
+
+  DearImGui.popItemWidth
                         
